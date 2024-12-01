f1e8eb0417c7fd6908671e93838dacf7c041d63e
LitFill <marrazzy54@gmail.com>
2024-12-01 23:55:59 +0700

    feat(01): tulis ulang kode day1

--- 01/Main.hs
+++ 01/Main.hs
@@ -1,6 +1,55 @@
 module Main where
 
+import Data.Functor ((<&>))
+import Data.List (sort)
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
+import Data.Tuple.Extra (both, second)
+import Flow
+
+parseInput :: String -> [(Int, Int)]
+parseInput str =
+  lines str
+    <&> words
+      .> (\[a, b] -> (a, b))
+      .> both (read @Int)
+
+part1 :: String -> Int
+part1 =
+  parseInput
+    .> unzip
+    .> both sort
+    .> uncurry zip
+    .> map (uncurry dist)
+    .> sum
+
+dist :: Int -> Int -> Int
+dist a b = abs $ a - b
+
+part2 =
+  parseInput
+    .> unzip
+    .> second mapFromList
+    .> weightedSum
+
+weightedSum :: (Ord b, Num b) => ([b], Map b b) -> b
+weightedSum (xs, m) =
+  xs
+    |> map (\x -> Map.findWithDefault 0 x m * x)
+    |> sum
+
+mapFromList :: (Ord k, Num v) => [k] -> Map k v
+mapFromList = foldl insert Map.empty
+
+insert :: (Ord k, Num a) => Map k a -> k -> Map k a
+insert acc k = Map.insertWith (+) k 1 acc
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  putStrLn input
+  -- \| jawaban part 1
+  putStr "Part 1: "
+  print $ part1 input
+  -- \| jawaban part 2
+  putStr "Part 2: "
+  print $ part2 input
